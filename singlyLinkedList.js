3cf822816924abdeac68551f73c8776c111726f9
Slavko Tretiak <sti-web@users.noreply.github.com>
2024-01-18 21:40:32 +0100

    Added reverse

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -91,6 +91,28 @@ class LinkedList {
   }
 
   /**
+   * Reverse all of the nodes
+   * @returns
+   */
+  reverse() {
+    if (!this.head.next) {
+      return this.head;
+    }
+    let first = this.head;
+    this.tail = this.head;
+    let second = first.next;
+    while (second) {
+      const temp = second.next;
+      second.next = first;
+      first = second;
+      second = temp;
+    }
+    this.head.next = null;
+    this.head = first;
+    return this;
+  }
+
+  /**
    * Find a node at given index
    * @param {Number} index
    * @returns
